68e85f44f0d4df0db220d30133bfb6a1c43ac7ec
lihaoyi <haoyi.sg@gmail.com>
2013-07-25 11:19:19 +0800

    shorthand functions in Misc now take Any instead of String

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ organization  := "com.scalatags"
 
 name          := "scalatags"
 
-version       := "0.1.1"
+version       := "0.1.2"
 
 scalaVersion  := "2.10.0"
 
--- src/main/scala/scalatags/Misc.scala
+++ src/main/scala/scalatags/Misc.scala
@@ -10,13 +10,13 @@ private[scalatags] trait Misc{
   /**
    * Shorthand to generate a <script type="text/javascript" src="..."></script> tag
    */
-  def javascript(origin: String = "") =
+  def javascript(origin: Any = "") =
     script.attr("type" -> "text/javascript").src(origin)("")
 
   /**
    * Shorthand to generate a <style rel="stylesheet" type="text/css" href="..."></style> tag
    */
-  def stylesheet(origin: String = "") =
+  def stylesheet(origin: Any = "") =
     link.rel("stylesheet").ctype("text/css").href(origin)
 
 }
