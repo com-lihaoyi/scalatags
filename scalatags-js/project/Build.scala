9bec043c0ad73df35e030bc01de276eccf358a67
Peluko <peluko@peluko.net>
2013-12-26 16:47:00 +0100

    Added project for use with Scala.js

---
+++ scalatags-js/project/Build.scala
@@ -0,0 +1,14 @@
+import sbt._
+import Keys._
+import scala.scalajs.sbtplugin.ScalaJSPlugin._
+import ScalaJSKeys._
+object Build extends sbt.Build {
+  lazy val root = project.in(file("."))
+    .settings(scalaJSSettings: _*)
+    .settings(
+      name := "scalatags-js",
+      version := "0.1.4",
+      unmanagedSourceDirectories in Compile += baseDirectory.value / ".." / "src" / "main", // test sources are omitted from js build
+      excludeFilter in Compile := "Tags.scala" // ommit the original file (symbols version), will compile TagsJS.scala instead
+    )
+}
