46127f40eb92c3b63fb658450da5bf6a8d3e27d2
Richard Gomes <rgomes.info@gmail.com>
2014-07-28 09:57:40 +0100

    Bump version 0.3.8-1

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object Build extends sbt.Build{
     ),
 
     // Sonatype
-    version := "0.3.8",
+    version := "0.3.8-1",
     publishTo <<= version { (v: String) =>
       Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
     },
