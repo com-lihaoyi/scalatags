259808a07c69d1cd9411f75537541a6b155036d6
Mark Lister <mark.p.lister@gmail.com>
2015-04-17 08:36:18 +0200

    Issue #76 Native pretty print

--- scalatags/shared/src/main/scala/scalatags/Text.scala
+++ scalatags/shared/src/main/scala/scalatags/Text.scala
@@ -198,7 +198,51 @@ object Text
       writeTo(strb)
       strb.toString()
     }
+
     def render: Output = this.toString.asInstanceOf[Output]
+
+    def prettyWriteTo(t: Text.TypedTag[String], strb: StringBuilder, depth: Int, step: Int): Unit = {
+      val indent = " " * depth * step
+      val builder = new text.Builder()
+      t.build(builder)
+      val attrs = builder.attrs.take(builder.attrIndex)
+      val children = builder.children.take(builder.childIndex).toList
+
+      def escape(s: String): String = {
+        val sb = new StringBuilder
+        Escaping.escape(s, sb)
+        sb.toString()
+      }
+
+      strb ++= indent += '<' ++= t.tag
+
+      strb ++= attrs.map(a => " " + a._1 + " = \"" + escape(a._2) + "\" ").mkString
+
+      if (children.isEmpty && t.void) {
+        strb ++= "/>"
+      } else {
+        strb ++= ">"
+        for (c <- children) {
+          c match {
+            case t: TypedTag[String] =>
+              strb ++= "\n"
+              prettyWriteTo(t, strb, depth + 1, step)
+            case any =>
+              strb ++= "\n" ++= " " * (depth + 1) * step
+              any.writeTo(strb)
+          }
+        }
+        strb ++= "\n" ++= indent ++= "</" ++= t.tag += '>'
+      }
+    }
+
+    def pretty (step: Int = 4): String = {
+      val strb = new StringBuilder
+      prettyWriteTo (this, strb, 0, step)
+      strb.toString()
+    }
+
+    def pretty: String = pretty()
   }
 
 
