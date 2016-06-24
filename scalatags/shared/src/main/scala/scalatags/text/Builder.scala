70331cd6756e5d6d72d540606b9f01d00a841dcc
Rory Graves <rory.graves@fieldmark.co.uk>
2016-06-24 10:59:00 +0100

    Performance improvements to rendering

--- scalatags/shared/src/main/scala/scalatags/text/Builder.scala
+++ scalatags/shared/src/main/scala/scalatags/text/Builder.scala
@@ -13,9 +13,37 @@ import scala.reflect.ClassTag
  * so even though the stuff isn't private, don't touch it!
  */
 class Builder(var children: Array[Frag] = new Array(4),
-              var attrs: Array[(String, String)] = new Array(4)){
+              var attrs: Array[(String, AttrValueSource)] = new Array(4)){
   final var childIndex = 0
   final var attrIndex = 0
+
+  private[this] def incrementChidren(arr: Array[Frag], index: Int) = {
+    if (index >= arr.length){
+      val newArr = new Array[Frag](arr.length * 2)
+      var i = 0
+      while(i < arr.length){
+        newArr(i) = arr(i)
+        i += 1
+      }
+      newArr
+    }else{
+      null
+    }
+  }
+
+  private[this] def incrementAttr(arr: Array[(String, AttrValueSource)], index: Int) = {
+    if (index >= arr.length){
+      val newArr = new Array[(String, AttrValueSource)](arr.length * 2)
+      var i = 0
+      while(i < arr.length){
+        newArr(i) = arr(i)
+        i += 1
+      }
+      newArr
+    }else{
+      null
+    }
+  }
 
   private[this] def increment[T: ClassTag](arr: Array[T], index: Int) = {
     if (index >= arr.length){
@@ -31,37 +59,51 @@ class Builder(var children: Array[Frag] = new Array(4),
     }
   }
   def addChild(c: Frag) = {
-    val newChildren = increment(children, childIndex)
+    val newChildren = incrementChidren(children, childIndex)
     if (newChildren != null) children = newChildren
     children(childIndex) = c
     childIndex += 1
   }
-  def appendAttr(k: String, v: String) = {
+  def appendAttr(k: String, v: AttrValueSource) = {
 
     attrIndex(k) match{
       case -1 =>
-        val newAttrs = increment(attrs, attrIndex)
+        val newAttrs = incrementAttr(attrs, attrIndex)
         if (newAttrs!= null) attrs = newAttrs
 
         attrs(attrIndex) = k -> v
         attrIndex += 1
       case n =>
         val (oldK, oldV) = attrs(n)
-        attrs(n) = (oldK, oldV + v)
+        attrs(n) = (oldK, ChainedAttributeValueSource(oldV, v))
     }
   }
-  def setAttr(k: String, v: String) = {
+  def setAttr(k: String, v: AttrValueSource) = {
     attrIndex(k) match{
       case -1 =>
-        val newAttrs = increment(attrs, attrIndex)
+        val newAttrs = incrementAttr(attrs, attrIndex)
         if (newAttrs!= null) attrs = newAttrs
         attrs(attrIndex) = k -> v
         attrIndex += 1
       case n =>
         val (oldK, oldV) = attrs(n)
-        attrs(n) = (oldK, v)
+        attrs(n) = (oldK, ChainedAttributeValueSource(oldV, v))
     }
   }
+
+
+  def appendAttrStrings(v: AttrValueSource, sb: StringBuilder): Unit = {
+    v.appendAttrValue(sb)
+  }
+
+  def attrsString(v: AttrValueSource): String = {
+    val sb = new StringBuilder
+    appendAttrStrings(v, sb)
+    sb.toString
+  }
+
+
+
   def attrIndex(k: String): Int = {
     attrs.indexWhere(x => x != null && x._1 == k)
   }
