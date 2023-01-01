ca8352647899747612ac8076d30830f664ec58c0
Chris Kipp <ckipp@pm.me>
2023-01-01 13:05:52 +0100

    fix: ensure defs are captured in Scala 3 stylesheets
    In Scala 2 the macro for SourceClasses captures both `def`s and `val`s
    where currently the Scala 3 implementation will only capture `val`s.
    This change updates the Scala 3 `SourceClasses` to ensure parity with
    the Scala 2 implementation making sure to also capture `def` classes.
    
    Fixes #257

--- scalatags/src-3/scalatags/stylesheet/SourceClasses.scala
+++ scalatags/src-3/scalatags/stylesheet/SourceClasses.scala
@@ -3,17 +3,21 @@ package scalatags.stylesheet
 import scala.quoted.*
 
 class SourceClasses[T](val value: T => Seq[Cls])
-object SourceClasses {
+object SourceClasses:
   inline implicit def apply[T]: SourceClasses[T] = ${ manglerImpl[T] }
 
-  def manglerImpl[T: Type](using Quotes): Expr[SourceClasses[T]] = {
+  def manglerImpl[T: Type](using Quotes): Expr[SourceClasses[T]] =
     '{ new SourceClasses[T]((t: T) => ${ Expr.ofSeq(terms('t)) }) }
-  }
 
-  private def terms[T: Type](t: Expr[T])(using ctx: Quotes): Seq[Expr[Cls]] = {
+  private def terms[T: Type](t: Expr[T])(using ctx: Quotes): Seq[Expr[Cls]] =
     import ctx.reflect._
     val valdefs = TypeRepr.of[T].typeSymbol.declaredFields.map(_.tree.asInstanceOf[ValDef])
-    val clsesOnly: Seq[ValDef] = valdefs.filter(_.tpt.tpe =:= TypeRepr.of[Cls])
-    clsesOnly.map(valdef => t.asTerm.select(valdef.symbol).asExprOf[Cls])
-  }
-}
+    val defdefs = TypeRepr.of[T].typeSymbol.declaredMethods.map(_.tree.asInstanceOf[DefDef])
+
+    val valclsesOnly: Seq[ValDef] = valdefs.filter(_.tpt.tpe =:= TypeRepr.of[Cls])
+    val defclsesOnly: Seq[DefDef] = defdefs.filter(_.returnTpt.tpe =:= TypeRepr.of[Cls])
+
+    val defClsExpressions = defclsesOnly.map(defdef => t.asTerm.select(defdef.symbol).asExprOf[Cls])
+    val valClsExpressions = valclsesOnly.map(valdef => t.asTerm.select(valdef.symbol).asExprOf[Cls])
+
+    defClsExpressions ++ valClsExpressions
